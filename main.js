7fe3c88f2c56896a72010d76ae2315248158855f
Philippe <philippe.idalgoprestes@gmail.com>
2026-05-08 01:11:33 -0300

    stats loading

--- main.js
+++ main.js
@@ -23,6 +23,8 @@ export function randomName(names){
     return(name)
 }
 
+window.randomName = randomName
+
 // Cria a style tag e a popula com reset.css
 document.getElementsByTagName("head")[0].appendChild(document.createElement("style"))
 const resetCss = `
