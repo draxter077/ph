90d6865fb5ae5df3dfa15527865bc25e4673934c
Philippe <philippe.idalgoprestes@gmail.com>
2026-05-07 21:52:54 -0300

    m

--- pages/main/method/main.js
+++ pages/main/method/main.js
@@ -25,7 +25,7 @@ export default function method(){
     method.appendChild(items([
         {img:"/assets/icons/google.png",title:"Otimizado para SEO/Google",description:"Você estará onde seus clientes estão"},
         {img:"/assets/icons/user.png",title:"Baseado nas últimas recomendações de UX",description:"Sua empresa não para no tempo. Seu site também não"},
-        {img:"/assets/icons/deal.png",title:"Focado em geração de leads",description:"Afinal, não adianta só ser bonito"},
+        {img:"/assets/icons/deal.png",title:"Focado em geração de leads",description:"Não basta ser um site bonito"},
         {img:"/assets/icons/responsive.png",title:"Design responsivo",description:"Para todos os clientes"},
         {img:"/assets/icons/code.png",title:"Leve e rápido",description:"Seu cliente não terá tempo de pensar na concorrência"},
         {img:"/assets/icons/marketing.png",title:"Gerador de tráfego orgânico",description:"Sua marca naturalmente sendo vista por quem você quer"},
